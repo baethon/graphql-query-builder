906e77f54aa29406c97793d5e6436ff564c1bd50
Radoslaw Mejer <radmen@baethon.com>
2023-08-23 23:16:54 +0200

    Use dataset in IntegrationTest

--- tests/Integration/IntegrationTest.php
+++ tests/Integration/IntegrationTest.php
@@ -1,24 +1,33 @@
 <?php
 
 use Symfony\Component\Finder\Finder;
+use Symfony\Component\Finder\SplFileInfo;
 
 $finder = (Finder::create())
     ->in(__DIR__)
     ->name('*.gql')
     ->files();
 
-foreach ($finder as $item) {
+$dataset = array_reduce(
+    iterator_to_array($finder),
+    function ($carry, SplFileInfo $item) {
+        return [
+            ...$carry,
+            $item->getBasename() => $item,
+        ];
+    },
+    []
+);
+
+it('generates query', function (SplFileInfo $item) {
     $query = require __DIR__.'/'.$item->getFilenameWithoutExtension().'.php';
+    $expected = preg_replace(
+        "/^\s+/m",
+        '',
+        trim($item->getContents()),
+    );
 
-    test($item->getBasename(), function () use ($item, $query) {
-        $expected = preg_replace(
-            "/^\s+/m",
-            '',
-            trim($item->getContents()),
-        );
+    $actual = trim((string) $query);
 
-        $actual = trim((string) $query);
-
-        expect($actual)->toEqual($expected);
-    })->skip(is_null($query), 'Missing implementation');
-}
+    expect($actual)->toEqual($expected);
+})->with($dataset);
